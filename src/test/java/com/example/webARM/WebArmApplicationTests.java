8914f02ed596687bd78307a20f3fa6800335804e
revinsd <revinsd456@gmail.com>
2022-07-05 18:53:28 +0500

    initial commit

---
+++ src/test/java/com/example/webARM/WebArmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.webARM;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebArmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
